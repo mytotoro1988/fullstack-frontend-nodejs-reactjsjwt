aa335e1efec2b1b827c1410c3433b447e214ce09
cuong <lehuucuong.vnua@gmail.com>
2024-09-20 14:13:07 +0700

    config cite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3002, // Change this to your desired port
-  },
   plugins: [react()],
+  build: {
+    outDir: "dist", // Thư mục xuất file build
+    sourcemap: true, // Tạo sourcemap để hỗ trợ debug trong môi trường production
+  },
+  server: {
+    port: 3002, // Cổng chạy server dev
+    open: true, // Tự động mở trình duyệt khi chạy server dev
+  },
+  preview: {
+    port: 3003, // Cổng chạy server preview (nếu dùng lệnh 'vite preview')
+    open: true,
+  },
 });
