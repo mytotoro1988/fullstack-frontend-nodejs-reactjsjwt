cf1741545139593d2d5569112eccf2a1269d357a
Cuong <lehuucuong.vnua@gmail.com>
2024-09-21 19:37:01 +0700

    300

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   preview: {
-    port: 3004, // Cổng chạy server preview (nếu dùng lệnh 'vite preview')
+    port: 3000, // Cổng chạy server preview (nếu dùng lệnh 'vite preview')
     open: true,
   },
   build: {
